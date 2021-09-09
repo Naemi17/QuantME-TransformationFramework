483077ce4c711bdd0340b0f740f622f9390ddd02
tjehret <tjehret@gmx.de>
2021-09-09 12:32:48 +0200

    Add ScriptSplitterPlugin initials files

---
+++ resources/plugins/QuantME-CamundaPlugin/scriptSplitter/ScriptSplitterPlugin.js
@@ -0,0 +1,24 @@
+import React, { Fragment, PureComponent } from 'camunda-modeler-plugin-helpers/react';
+import { Modal } from 'camunda-modeler-plugin-helpers/components';
+import { Fill } from 'camunda-modeler-plugin-helpers/components';
+
+import {createMyShapes} from './Creator';
+
+
+export default class ScriptSplitterPlugin extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+
+    // render the button to start the splitting algorithm
+    return (<Fragment>
+      <Fill slot="toolbar">
+        <button type="button" className="src-app-primitives-Button__Button--3Ffn0" title="Start Splitting"
+            onClick={() => createMyShapes("")}>
+            <span className="workflow-transformation"><span className="indent">Execute ScriptSplitter</span></span>
+        </button>
+      </Fill>
+    </Fragment>);
+  }
+}
