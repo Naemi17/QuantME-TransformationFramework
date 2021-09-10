1d44da58d076740968a96bd1ad890eb722335837
tjehret <tjehret@gmx.de>
2021-09-10 17:46:52 +0200

    Update ScriptSplitterPlugin
    The Button is able to create shapes in the modeler to form a simple template.

--- resources/plugins/QuantME-CamundaPlugin/scriptSplitter/Creator.js
+++ resources/plugins/QuantME-CamundaPlugin/scriptSplitter/Creator.js
@@ -7,42 +7,52 @@
  */
 
 import { createModeler } from '../quantme/Utilities';
+import { layout } from 'client/src/app/quantme/layouter/Layouter';
+import { getRootProcess } from 'client/src/app/quantme/utilities/Utilities';
+/**
+* create my shapes here
+* e.g. this creates a template for the moment.
+* @param modeler the currently active modeler
+*/
+export function createTemplate(modeler) {
+  // initialize modeling helpers
+  let modeling = modeler.get('modeling');
+  let elementRegistry = modeler.get('elementRegistry');
+  let elementFactory = modeler.get('bpmnFactory');
+  // get root element of the current diagram and THE ROOT PROCESS
+  const definitions = modeler.getDefinitions();
+  const rootElement = getRootProcess(definitions);
+  var process = elementRegistry.get(rootElement.id);
+
+  // get the (default) start event
+  var startEvent = elementRegistry.get('StartEvent_1');
+  // create
+  var quantumTask = modeling.createShape({ type: 'bpmn:ScriptTask' }, { x: 100, y: 100 }, process, {});
+  var quantumTaskBo = elementRegistry.get(quantumTask.id).businessObject;
+  quantumTaskBo.name = 'Quantum Part';
+  var preprocessingTask = modeling.createShape({ type: 'bpmn:ScriptTask' }, { x: 100, y: 100 }, process, {});
+  var preprocessingTaskBo = elementRegistry.get(preprocessingTask.id).businessObject;
+  preprocessingTaskBo.name = 'Preprocessing part';
+  var postprocessingTask = modeling.createShape({ type: 'bpmn:ScriptTask' }, { x: 100, y: 100 }, process, {});
+  var postprocessingTaskBo = elementRegistry.get(postprocessingTask.id).businessObject;
+  postprocessingTaskBo.name = 'Postprocessing Part';
+  var endEvent = modeling.createShape({ type: 'bpmn:EndEvent' }, { x: 100, y: 100 }, process, {});
+  // connect
+  modeling.connect(startEvent, preprocessingTask, { type: 'bpmn:SequenceFlow' });
+  modeling.connect(preprocessingTask, quantumTask, { type: 'bpmn:SequenceFlow' });
+  modeling.connect(quantumTask, postprocessingTask, { type: 'bpmn:SequenceFlow' });
+  modeling.connect(postprocessingTask, endEvent, { type: 'bpmn:SequenceFlow' });
+  // make it pretty
+  layout(modeling, elementRegistry, rootElement);
+}
 
 /**
 * create my shapes here
 * e.g. this creates a template for the moment.
 * @param metaData metaData containing the infos about loops
 */
-export function createMyShapes(metaData){
-
-  let modeler = createModeler();
-  let modeling = modeler.get('modeling');
-  let elementRegistry = modeler.get('elementRegistry');
-  let elementFactory = modeler.get('bpmnFactory');
-
-  var process = elementRegistry.get('Process_1');
-  var startEvent = elementRegistry.get('StartEvent_1');
-  var endEvent = elementFactory.createShape({ type: 'bpmn:EndEvent' });
-  var preprocessingTask = elementFactory.createShape({ type: 'bpmn:ScripTask' });
-  var quantumTask = elementFactory.createShape({ type: 'bpmn:ScripTask'});
-  var postprocessingTask = elementFactory.createShape({ type: 'bpmn:ScripTask' });
-  var splittingGateway = elementFactory.createShape({ type: 'bpmn:ExclusiveGateway' });
-  var joiningGateway = elementFactory.createShape({ type: 'bpmn:ExclusiveGateway' });
-
-  modeling.createShape(splittingGateway, { x: 50, y: 50 }, process);
-  modeling.createShape(joiningGateway, { x: 50, y: 50 }, process);
-  modeling.createShape(preprocessingTask, { x: 50, y: 50 }, process);
-  modeling.createShape(quantumTask, { x: 50, y: 50 }, process);
-  modeling.createShape(postprocessingTask, { x: 50, y: 50 }, process);
-
-  // TODO handle loops according to splitting output
-  modeling.connect(startEvent, preprocessingTask);
-  modeling.connect(preprocessingTask, splittingGateway);
-  modeling.connect(splittingGateway, quantumTask);
-  modeling.connect(quantumTask, joiningGateway);
-  modeling.connect(joiningGateway, postprocessingTask );
-  modeling.connect(joiningGateway, splittingGateway);
-  modeling.connect(postprocessingTask, endEvent);
+export function createMyShapes(metaData) {
+  // TODO implement
 }
 
 /**
@@ -50,7 +60,7 @@ export function createMyShapes(metaData){
 * @param message the data for the test
 * @param props property element for displaying the message
 */
-export function test(message, props){
+export function test(message, props) {
   props.displayNotification({
     type:'warning',
     title: 'TEST',
--- resources/plugins/QuantME-CamundaPlugin/scriptSplitter/ScriptSplitterPlugin.js
+++ resources/plugins/QuantME-CamundaPlugin/scriptSplitter/ScriptSplitterPlugin.js
@@ -10,24 +10,54 @@ import React, { Fragment, PureComponent } from 'camunda-modeler-plugin-helpers/r
 import { Modal } from 'camunda-modeler-plugin-helpers/components';
 import { Fill } from 'camunda-modeler-plugin-helpers/components';
 
-import { createMyShapes, test } from './Creator';
+import { createTemplate } from './Creator';
 
 
 export default class ScriptSplitterPlugin extends PureComponent {
   constructor(props) {
     super(props);
   }
+  componentDidMount() {
+
+    // initialize component with created modeler
+    this.props.subscribe('bpmn.modeler.created', (event) => {
+
+      const {
+        modeler, tab
+      } = event;
+
+      // save modeler and activate as current modeler
+      this.modeler = modeler;
+      const self = this;
+
+      // register actions to enable invocation over the menu and the API
+      this.editorActions = modeler.get('editorActions');
+  });
+}
   render() {
 
     // render the button to start the splitting algorithm
     return (<Fragment>
       <Fill slot="toolbar">
         <button type="button" className="src-app-primitives-Button__Button--3Ffn0" title="Start Splitting"
-          onClick= {() => test('finally, i am able to import a function', this.props)}>
+          onClick= {() => this.startSplitting()}>
           <span className="workflow-transformation"><span className="indent">Execute ScriptSplitter</span></span>
         </button>
       </Fill>
     </Fragment>);
   }
 
+/**
+* start the algorithm
+*/
+startSplitting() {
+  var message = 'Start the Splitting...'
+  this.props.displayNotification({
+    type:'warning',
+    title: 'ScriptSplitter',
+    content: message,
+    duration: 10000
+    });
+    createTemplate(this.modeler);
+  }
 }
