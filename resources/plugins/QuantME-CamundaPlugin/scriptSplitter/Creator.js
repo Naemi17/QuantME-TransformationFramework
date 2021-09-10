80a5f4694184550f3ec27e0b1330a246d29b4ea6
tjehret <tjehret@gmx.de>
2021-09-10 12:20:55 +0200

    Update ScriptSplitter Plugin
    The Plugin is now able to create a button in the toolbar.
    Furthermore we can import a function and display some test messages.

--- resources/plugins/QuantME-CamundaPlugin/scriptSplitter/Creator.js
+++ resources/plugins/QuantME-CamundaPlugin/scriptSplitter/Creator.js
@@ -1,39 +1,61 @@
-import { createModeler } from "../quantme/Utilities";
+/**
+ * This program and the accompanying materials are made available under the
+ * terms the Apache Software License 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
+import { createModeler } from '../quantme/Utilities';
 
 /**
 * create my shapes here
 * e.g. this creates a template for the moment.
-* @param: metaData metaData containing the infos about loops
+* @param metaData metaData containing the infos about loops
 */
-export function createMyShapes(metaData){ 
+export function createMyShapes(metaData){
 
   let modeler = createModeler();
   let modeling = modeler.get('modeling');
   let elementRegistry = modeler.get('elementRegistry');
-  let elementFactory = modeler.get('bpmnFactory'); 
-  
-  const process = elementRegistry.get('Process_1');
-  const startEvent = elementRegistry.get('StartEvent_1');
-  const endEvent = elementFactory.createShape({ type: 'bpmn:EndEvent' });
-  const PreprocessingTask = elementFactory.createShape({ type: 'bpmn:ScripTask' });
-  const QuantumTask = elementFactory.createShape({ type: 'bpmn:ScripTask'});
-  const PostprocessingTask = elementFactory.createShape({ type: 'bpmn:ScripTask' });
-  const splittingGateway = elementFactory.createShape({ type: 'bpmn:ExclusiveGateway' });
-  const joiningGateway = elementFactory.createShape({ type: 'bpmn:ExclusiveGateway' });
+  let elementFactory = modeler.get('bpmnFactory');
+
+  var process = elementRegistry.get('Process_1');
+  var startEvent = elementRegistry.get('StartEvent_1');
+  var endEvent = elementFactory.createShape({ type: 'bpmn:EndEvent' });
+  var preprocessingTask = elementFactory.createShape({ type: 'bpmn:ScripTask' });
+  var quantumTask = elementFactory.createShape({ type: 'bpmn:ScripTask'});
+  var postprocessingTask = elementFactory.createShape({ type: 'bpmn:ScripTask' });
+  var splittingGateway = elementFactory.createShape({ type: 'bpmn:ExclusiveGateway' });
+  var joiningGateway = elementFactory.createShape({ type: 'bpmn:ExclusiveGateway' });
 
   modeling.createShape(splittingGateway, { x: 50, y: 50 }, process);
   modeling.createShape(joiningGateway, { x: 50, y: 50 }, process);
-  modeling.createShape(PreprocessingTask, { x: 50, y: 50 }, process);
-  modeling.createShape(QuantumTask, { x: 50, y: 50 }, process);
-  modeling.createShape(PostprocessingTask, { x: 50, y: 50 }, process);
+  modeling.createShape(preprocessingTask, { x: 50, y: 50 }, process);
+  modeling.createShape(quantumTask, { x: 50, y: 50 }, process);
+  modeling.createShape(postprocessingTask, { x: 50, y: 50 }, process);
 
   // TODO handle loops according to splitting output
-  modeling.connect(startEvent, PreprocessingTask);
-  modeling.connect(PreprocessingTask, splittingGateway);
-  modeling.connect(splittingGateway, QuantumTask);
-  modeling.connect(QuantumTask, joiningGateway);
-  modeling.connect(joiningGateway, PostprocessingTask );
+  modeling.connect(startEvent, preprocessingTask);
+  modeling.connect(preprocessingTask, splittingGateway);
+  modeling.connect(splittingGateway, quantumTask);
+  modeling.connect(quantumTask, joiningGateway);
+  modeling.connect(joiningGateway, postprocessingTask );
   modeling.connect(joiningGateway, splittingGateway);
-  modeling.connect(PostprocessingTask, endEvent);
+  modeling.connect(postprocessingTask, endEvent);
+}
+
+/**
+* simple test function that
+* @param message the data for the test
+* @param props property element for displaying the message
+*/
+export function test(message, props){
+  props.displayNotification({
+    type:'warning',
+    title: 'TEST',
+    content: message,
+    duration: 20000
+  });
+
 }
--- resources/plugins/QuantME-CamundaPlugin/scriptSplitter/ScriptSplitterPlugin.js
+++ resources/plugins/QuantME-CamundaPlugin/scriptSplitter/ScriptSplitterPlugin.js
@@ -1,8 +1,16 @@
+/**
+ * This program and the accompanying materials are made available under the
+ * terms the Apache Software License 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import React, { Fragment, PureComponent } from 'camunda-modeler-plugin-helpers/react';
 import { Modal } from 'camunda-modeler-plugin-helpers/components';
 import { Fill } from 'camunda-modeler-plugin-helpers/components';
 
-import { createMyShapes } from './Creator';
+import { createMyShapes, test } from './Creator';
 
 
 export default class ScriptSplitterPlugin extends PureComponent {
@@ -15,10 +23,11 @@ export default class ScriptSplitterPlugin extends PureComponent {
     return (<Fragment>
       <Fill slot="toolbar">
         <button type="button" className="src-app-primitives-Button__Button--3Ffn0" title="Start Splitting"
-          onClick={() => createMyShapes('')}>
+          onClick= {() => test('finally, i am able to import a function', this.props)}>
           <span className="workflow-transformation"><span className="indent">Execute ScriptSplitter</span></span>
         </button>
       </Fill>
     </Fragment>);
   }
+
 }
